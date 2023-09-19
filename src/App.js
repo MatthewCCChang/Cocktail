12c0e8875e8f801f3369a8d8d76f1dc14a5bfd38
Matthew <deadwalker1228@gmail.com>
2023-09-19 08:32:00 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import {useState, useCallback, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+  //have a text box then add to end of url
+  const fetchCocktailHandler = useCallback(() => {
+    setLoading(true);
+    axios.get(url+"Elderflower_cordial").then(res=>{
+      console.log(res.data);
+      setData(res.data.drinks);
+    }).catch(e=>console.log(e))
+    .finally(() => setLoading(false));
+  }, [])
+
+//.replace() for getting ingredients
+//Autocomplete and Textfield from MUI to get input
+  useEffect(() => {
+    fetchCocktailHandler();
+  }, [fetchCocktailHandler]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data.map((cocktail => (
+        <div key={cocktail.idDrink}>
+          <h2>{cocktail.strDrink}</h2>
+          <img src={cocktail.strDrinkThumb} alt="#"/>
+        </div>
+      )))}
     </div>
   );
+
 }
 
 export default App;
