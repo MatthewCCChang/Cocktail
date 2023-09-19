418db134f4a8a77b8d28b752ed7062bc09cfaca3
Matthew <deadwalker1228@gmail.com>
2023-09-19 11:53:55 -0700

    Added combobox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {useState, useCallback, useEffect} from 'react';
 import axios from 'axios';
+import { Autocomplete, TextField, Box } from '@mui/material';
 import './App.css';
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
+  const ingredients = [{name: "Gin"},{name: "Elderflower_cordial"}];
 
   const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
   //have a text box then add to end of url
@@ -24,6 +26,21 @@ function App() {
   }, [fetchCocktailHandler]);
   return (
     <div className="App">
+      <Autocomplete 
+        sx={{width:300, margin: 5, mx: 'auto'}}
+        autoHighlight
+        options = {ingredients}
+        renderOption={(props, option) => (
+          <Box>{option.name}</Box>
+        )}
+        renderInput={(params) => (
+          <TextField 
+            {...params}
+            label = "Choose an ingredient"
+            inputProps={{...params.inputProps, autocomplete: "new-password",}}
+          />
+        )}
+      />
       {data.map((cocktail => (
         <div key={cocktail.idDrink}>
           <h2>{cocktail.strDrink}</h2>
