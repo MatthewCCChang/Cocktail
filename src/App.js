b27710c07d89fa4372a0fd3a158e2ddd6a0de1f1
Matthew <deadwalker1228@gmail.com>
2023-09-21 15:38:42 -0700

    Wrote getIngredient() to grab all ingredients into selection box

--- src/App.js
+++ src/App.js
@@ -6,13 +6,31 @@ import './App.css';
 function App() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
-  
+  const [ingredients3, setIngredients] = useState([]);
+  const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+
+  const getIngredients = useCallback(() => {
+    axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").then(res=>{
+      setIngredients(res.data.drinks);
+    }).catch(e => console.log(e))
+  }, [])
+  console.log(ingredients3);
+  var counter =-1;
+  const modified_ingredients = ingredients3.map(drink => {
+    counter++;
+    if(drink.strIngredient1.includes(" ")){
+      return {strIngredient1: drink.strIngredient1.replaceAll(" ", "_"), id:{counter} }
+    }
+    return {...drink, id: {counter}}
+  })
+  console.log(modified_ingredients);
+
   const ingredients = [{name: "Gin", id: 1},{name: "Elderflower_cordial", id: 2}];
 
-  const [value, setValue] = useState(ingredients[0]);
+  const [value, setValue] = useState(modified_ingredients[0]);
   const [inputValue, setInputValue] = useState('');
 
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+  
   //have a text box then add to end of url
   const fetchCocktailHandler = useCallback((type) => {
     setLoading(true);
@@ -26,15 +44,16 @@ function App() {
 //.replace() for getting ingredients
 //Autocomplete and Textfield from MUI to get input
   useEffect(() => {
+    getIngredients();
     fetchCocktailHandler("Gin");
-  }, [fetchCocktailHandler]);
+  }, [fetchCocktailHandler, getIngredients]);
   return (
     <div className="App">
       <Autocomplete 
         sx={{width:300, margin: 5, mx: 'auto'}}
         autoHighlight
-        options = {ingredients}
-        getOptionLabel={option => (option.name)}
+        options = {modified_ingredients}
+        getOptionLabel={option => (option.strIngredient1)}
         renderInput={(params) => (
           <TextField 
             {...params}
@@ -42,14 +61,14 @@ function App() {
             inputProps={{...params.inputProps, autoComplete: "new-password",}}
           />
         )}
-        isOptionEqualToValue={(option, value) => option.name===value.name}
+        isOptionEqualToValue={(option, value) => option.strIngredient1===value.strIngredient1}
         value={value}
         onChange={(event, newValue) => {
           setValue(newValue)
           
           if(newValue){
             console.log("not null")
-            fetchCocktailHandler(newValue.name)
+            fetchCocktailHandler(newValue.strIngredient1)
           }
           
         }}
