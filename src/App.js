f9f526631f0657583972258940f9bf1040c3809f
Matthew <deadwalker1228@gmail.com>
2024-01-25 21:45:34 -0800

    Finished recipe modal popup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { useState, useCallback, useEffect } from 'react';
-import axios from 'axios';
-import AutocompleteInput from './autocomplete';
-import CocktailList from './list';
-import './App.css';
+import { useState, useCallback, useEffect } from "react";
+import axios from "axios";
+import AutocompleteInput from "./autocomplete";
+import CocktailList from "./list";
+import "./App.css";
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   const [ingredients, setIngredients] = useState([]);
   const [value, setValue] = useState(null);
-  const [inputValue, setInputValue] = useState('');
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
-
+  const [inputValue, setInputValue] = useState("");
+  const url = process.env.REACT_APP_URL;
   const getIngredients = useCallback(() => {
-    axios.get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
-      .then(res => setIngredients(res.data.drinks))
-      .catch(e => console.log(e));
+    axios
+      .get("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list")
+      .then((res) => setIngredients(res.data.drinks))
+      .catch((e) => console.log(e));
   }, []);
 
   useEffect(() => {
     getIngredients();
   }, [getIngredients]);
-
   const fetchCocktailHandler = useCallback((type) => {
     setLoading(true);
-    axios.get(url + type)
-      .then(res => setData(res.data.drinks))
-      .catch(e => console.log(e))
+    axios
+      .get(url + type)
+      .then((res) => setData(res.data.drinks))
+      .catch((e) => console.log(e))
       .finally(() => setLoading(false));
   }, []);
 
--- src/card.js
+++ src/card.js
@@ -1,12 +1,102 @@
-import React from 'react';
-import './card.css';
-import { Button } from '@mui/material';
+import React, { useState } from "react";
+import { Button, Modal, Box, Typography, createTheme } from "@mui/material";
+import axios from "axios";
 
-const CocktailCard = ({ cocktail }) => (
-  <div className="cocktail-card">
-    <h2>{cocktail.strDrink}</h2>
-    <Button><img src={cocktail.strDrinkThumb+"/preview"} alt="#" /></Button>
-  </div>
-);
+const url = process.env.REACT_APP_RECIPE_URL;
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 200,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+const theme = createTheme({
+  typography: {
+    poster: {
+      fontSize: "4rem",
+    },
+    // Disable h2 variant
+    h2: undefined,
+  },
+});
+
+const CocktailCard = ({ cocktail }) => {
+  const [open, setOpen] = React.useState(false);
+  const [recipe, setRecipe] = useState();
+  const [ingredients, setIngredients] = useState([]);
+
+  const handleOpen = () => {
+    displayRecipe(cocktail);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const displayRecipe = (cocktail) => {
+    if (cocktail) {
+      axios
+        .get(url + cocktail.idDrink)
+        .then((res) => {
+          console.log(res);
+          const newRecipe = res.data.drinks[0];
+          setRecipe(newRecipe);
+
+          let ing = [];
+          for (let i = 1; i <= 15; i++) {
+            const ingredient = newRecipe && newRecipe[`strIngredient${i}`];
+            const amount = newRecipe && newRecipe[`strMeasure${i}`];
+
+            if (ingredient && amount) {
+              ing.push(
+                <Typography
+                  key={i}
+                  id="modal-recipe-description"
+                  variant="poster"
+                  component="h2"
+                >
+                  {amount} {ingredient}
+                </Typography>
+              );
+            }
+          }
+          setIngredients(ing);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    } else {
+      console.log("hi");
+    }
+  };
+
+  return (
+    <div className="cocktail-card">
+      <h2>{cocktail.strDrink}</h2>
+      <Button onClick={handleOpen} name={cocktail.id}>
+        <img src={cocktail.strDrinkThumb + "/preview"} alt="#" />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-recipel-title"
+        aria-describedby="modal-recipe-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-recipe-title" variant="h6" component="h2">
+            {cocktail.strDrink}
+          </Typography>
+          {ingredients}
+        </Box>
+      </Modal>
+    </div>
+  );
+};
 
 export default CocktailCard;
