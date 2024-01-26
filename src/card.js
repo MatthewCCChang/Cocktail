3081d88a60bacbdfa86bf0b2153288924cd38ae4
Matthew <deadwalker1228@gmail.com>
2024-01-25 18:01:44 -0800

    Changed image to image button

--- src/card.js
+++ src/card.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './card.css';
+import { Button } from '@mui/material';
 
 const CocktailCard = ({ cocktail }) => (
   <div className="cocktail-card">
     <h2>{cocktail.strDrink}</h2>
-    <img src={cocktail.strDrinkThumb+"/preview"} alt="#" />
+    <Button><img src={cocktail.strDrinkThumb+"/preview"} alt="#" /></Button>
   </div>
 );
 
